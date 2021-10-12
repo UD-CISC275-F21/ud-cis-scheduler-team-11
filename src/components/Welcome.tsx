8c61613085558cdf55a17a5bf4d9d43f681ce162
strrules105 <camobot3@gmail.com>
2021-10-12 14:49:16 -0400

    Removed forked repository, created new branch, adding name and Welcome here

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function Welcome(): JSX.Element {
+    return(
+        <div style={styles.welcome}>
+            <p>UD CIS Scheduler</p>
+        </div>
+    );
+}
+
+const styles = {
+    welcome: {
+        width: "80%",
+        padding: "0px 0px",
+        margin: "50px auto",
+        background: "white",
+        borderRadius: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontSize: 30,
+    },
+} as const;
