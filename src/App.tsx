a00497b62e9f475c36abd2b30967bba8f1d6fdb0
strrules105 <camobot3@gmail.com>
2021-10-12 14:08:21 -0400

    Added componenet Welcome with text "UD CIS Scheduler" because the linter test requires it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Names } from "./components/Names";
+import { Welcome } from "./components/Welcome";
 
 function App(): JSX.Element {
     return (
         <>
+            <Welcome></Welcome>
             <Names></Names>
         </>
     );
