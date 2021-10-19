350665770297cf95ebe4b815d3dad7fb152a7cc3
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-10-19 11:46:16 -0400

    Create Tables.tsx

---
+++ src/components/Tables.tsx
@@ -0,0 +1,23 @@
+import {Table} from 'react'
+
+export function Tables(): JSX.Element{
+    return <Table bordered>
+        <tbody>
+            <tr>
+                <td>Semester1</td>
+            </tr>
+            <tr>
+                <td>Semester2</td>
+            </tr>
+            <tr>
+                <td>College Year</td>
+            </tr>
+            <tr>
+                <td>Major</td>
+            </tr>
+            <tr>
+                <td>Graduation Year</td>
+            </tr>
+        </tbody>
+    </Table>
+}
