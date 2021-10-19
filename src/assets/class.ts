03b7c4f0d4c458e64208dacfd57bda47a3ef493d
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-10-19 12:16:14 -0400

    Create class.ts

---
+++ src/assets/class.ts
@@ -0,0 +1,9 @@
+[
+    {
+      "EGGG101: Introduction to Engineering(FYE)": 2,
+      "CISC108: Introduction to Computer Science 1": 3,
+      "MATH241: Analytic Geometry & Calculus A": 4,
+      "ENGL110: Seminar in Composition": 3,
+      "Breadth Requirement": 3
+    }
+  ]
