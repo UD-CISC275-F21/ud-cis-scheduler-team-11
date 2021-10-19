0ab7b23bb6e9b09680238a5d0a6560696b7d2992
strrules105 <camobot3@gmail.com>
2021-10-19 10:47:39 -0400

    Added a dropdown box that allows a user to select any of the 9 CS Bachelors degrees. The goal is to dynamically load courses for each of these majors.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { Names } from "./components/Names";
 import { Welcome } from "./components/Welcome";
+import { Dropdown } from "./components/CourseDropdown";
 
 function App(): JSX.Element {
     return (
         <>
             <Welcome></Welcome>
+            <Dropdown></Dropdown>
             <Names></Names>
         </>
     );
