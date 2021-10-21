25e53e537c135432fc42f99f4efe12ba373754c2
jameswan141 <70609875+jameswan141@users.noreply.github.com>
2021-10-21 10:46:14 -0400

    commit on "jameswan141-add-table"

---
+++ src/components/classlist.ts
@@ -0,0 +1,4 @@
+var names:string[]; 
+names = ["Introduction to Engineering","Intro to Computer Science 1","Analytic Geometry & Calculus A","Seminar in Composition","Humanities breadth requirement"] 
+console.log(alphas[0]); 
+console.log(alphas[1]);
