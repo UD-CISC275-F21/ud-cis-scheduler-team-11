2dbe0ca12fea5242d30a7eabc434d1cd42f3cc60
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-11-01 02:05:34 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer(): JSX.Element {
     return(
         <div style={styles.footer}>
             <p>Made for CISC275 at the University of Delaware</p>
-            <p>&copy; 2021 Tyler Rust, Noah Trungold, James Won</p>
+            <p>&copy; 2021 Tyler Rust, Noah Trungold, James Wan</p>
         </div>
     );
 }
