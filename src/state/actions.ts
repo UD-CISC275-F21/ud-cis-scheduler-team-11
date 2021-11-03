b84bb42505d5404029b98927d2022bdab0d67ff7
Noah Trungold <trungold@udel.edu>
2021-11-02 22:41:23 -0400

    implementing the action part of the process for adding elements and updating state

---
+++ src/state/actions.ts
@@ -0,0 +1,43 @@
+//an action now can resort to one of the forms we passed in, the list or task
+export type Action =
+    | {
+        type: "ADD_LIST"
+        payload: string
+    }
+    | {
+        type: "ADD_TASK"
+        payload: { text: string; listId: string }
+    }
+    
+    /*what I had before I decided to use a discriminated union which
+    allows the Typescript to look at the property and understand the other fields
+        interface AddListAction {
+            type: "ADD_LIST"
+            payload: string
+        }
+
+        interface AddTaskAction {
+            type: "ADD_LIST"
+            payload: { text: string; listId: string }
+        }
+
+        type Action = AddListAction | AddTaskAction
+
+    */
+export const addTask = (
+    text: string,
+    listId: string,
+): Action => ({
+    type: "ADD_TASK",
+    payload: {
+        text,
+        listId
+    }
+})
+
+export const addList = (
+    text: string,
+): Action => ({
+    type: "ADD_LIST",
+    payload: text
+})
