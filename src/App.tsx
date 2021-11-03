d92a2478e24ab5dd9802c17f09dabba4539b1dfe
Noah Trungold <trungold@udel.edu>
2021-11-02 22:29:55 -0400

    adding in changes to App and prep for other additions

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ import { SemesterButton } from "./components/SemesterButton";
 import "./components/Message.css";
 import { Message } from "./components/Message";
 import { Footer } from "./components/Footer";
+import { Column } from "./Column"
+import { AppContainer } from "./styles"
+import { AddNewItem } from "./AddNewItem"
+import { useAppState } from "./state/AppStateContext"
+import { addList } from "./state/actions"
 
 function App(): JSX.Element {
     return (
@@ -26,6 +31,18 @@ function App(): JSX.Element {
 }
 
 
-
-
-export default App;
+export const App = () => {
+    const {lists, dispatch} = useAppState()
+  
+    return (
+      <AppContainer>
+        {lists.map((list) => (
+          <Column text={list.text} key={list.id} id={list.id} />
+        ))}
+        <AddNewItem
+          toggleButtonText="+ Add another list"
+          onAdd={text => dispatch(addList(text))}
+        />
+      </AppContainer>
+    )
+  }
