8b4320e69848557bf318a08a8679838d524b9c5a
strrules105 <camobot3@gmail.com>
2021-11-03 15:17:55 -0400

    fixed bug

--- src/utils/useItemDrag.ts
+++ src/utils/useItemDrag.ts
@@ -4,7 +4,7 @@ import { DragItem } from "../DragItem";
 import { setDraggedItem } from "../state/actions";
 import { getEmptyImage } from "react-dnd-html5-backend";
 import { useEffect } from "react";
-export const useItemDrag = (item: DragItem) => {
+export const useItemDrag = (item: DragItem):  => {
     const { dispatch } = useAppState();
     const [, drag, preview] = useDrag({
         type: item.type,
