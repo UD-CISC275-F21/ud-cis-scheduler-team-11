a3fb52e42da975ac4048b515c57588de134d888c
Noah Trungold <trungold@udel.edu>
2021-11-02 22:31:49 -0400

    adding in the column container

---
+++ src/Column.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ColumnContainer, ColumnTitle } from "./styles";
+import {FC} from "react";
+import { AddNewItem } from "./AddNewItem"
+import { useAppState } from "./state/AppStateContext"
+import { Card } from "./Card";
+import { addTask } from "./state/actions"
+
+type ColumnProps = {
+    text: string;
+    id: string
+}
+
+export const Column = ({ text, id }: ColumnProps) => {
+    const { getTasksByListId } = useAppState()
+    const tasks = getTasksByListId(id)
+    return (
+        <ColumnContainer>
+            <ColumnTitle>{text}</ColumnTitle>
+            {tasks.map(task => (
+                <Card text={task.text} key={task.id} id={task.id} />
+            ))}
+            <AddNewItem
+                toggleButtonText="+ Add another task"
+                onAdd={console.log}
+                dark
+            />
+        </ColumnContainer>
+    )
+}
