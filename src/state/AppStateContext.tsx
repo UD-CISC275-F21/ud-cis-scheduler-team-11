15a1ef3e1e4387faffb82d1177b4ccdc7a4b2509
Noah Trungold <trungold@udel.edu>
2021-11-02 23:23:01 -0400

    removing previous work on app state

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -36,18 +36,6 @@ type AppStateContextProps = {
     dispatch: Dispatch<Action>
 }
     
-type Task = {
-    id: string
-    text: string
-}
-type List = {
-    id: string
-    text: string
-    tasks: Task[]
-}
-export type AppState = {
-    lists: List[]
-}
 //making sure we can access data globally
 export const useAppState = () => {
     return useContext(AppStateContext);
