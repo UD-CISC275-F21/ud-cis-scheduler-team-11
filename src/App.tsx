7ddedf1464587e821f969e9e59b9dad0415765fe
Noah Trungold <trungold@udel.edu>
2021-11-03 10:21:00 -0400

    adding tilt when moving

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { AppContainer } from "./styles";
 import { AddNewItem } from "./AddNewItem";
 import { useAppState } from "./state/AppStateContext";
 import { addList } from "./state/actions";
+import { CustomDragLayer } from "./CustomDragLayer";
 
 /*
 function App(): JSX.Element {
@@ -33,17 +34,18 @@ function App(): JSX.Element {
 */
 
 export const App = () => {
-    const {lists, dispatch} = useAppState();
-  
+    const { lists, dispatch } = useAppState();
+    
     return (
         <AppContainer>
+            <CustomDragLayer />
             {lists.map((list) => (
-                <Column text={list.text} key={list.id} id={list.id} />
+                <Column id={list.id} text={list.text} key={list.id} />
             ))}
             <AddNewItem
                 toggleButtonText="+ Add another list"
                 onAdd={text => dispatch(addList(text))}
             />
         </AppContainer>
-    );
-};
+    )
+}
--- src/styles.ts
+++ src/styles.ts
@@ -21,19 +21,20 @@ export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
     transform: ${props => (props.isPreview ? "rotate(5deg)" : undefined)};
     opacity: ${props => (props.isHidden ? 0 : 1)};
 `
+
 type DragPreviewWrapperProps = {
     position: {
         x: number
         y: number
     }
 }
-export const DragPreviewWrapper = styled.div.attrs < DragPreviewWrapperProps> (
-        ({ position: { x, y } }) => ({
-            style: {
-                transform: `translate(${x}px, ${y}px)`
-            }
-        })
-    ) <DragPreviewWrapperProps>``
+export const DragPreviewWrapper = styled.div.attrs<DragPreviewWrapperProps>(
+    ({ position: { x, y } }) => ({
+        style: {
+            transform: `translate(${x}px, ${y}px)`
+        }
+    })
+) <DragPreviewWrapperProps>``
 
 export const ColumnContainer = styled(DragPreviewContainer)`
     background-color: #ebecf0;
