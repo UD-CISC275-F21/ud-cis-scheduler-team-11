6032027f8f610dec8834f84649bdb2acac48bddb
strrules105 <camobot3@gmail.com>
2021-11-04 10:18:23 -0400

    Fixed linter issues

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,10 +7,10 @@ import { HTML5Backend as Backend } from "react-dnd-html5-backend";
 
 test("renders UD CIS Scheduler text", () => {
     render(<DndProvider backend={Backend}>
-                <AppStateProvider>
-                    <App />
-                </AppStateProvider>
-            </DndProvider>);
+        <AppStateProvider>
+            <App />
+        </AppStateProvider>
+    </DndProvider>);
     const welcomeMessage = screen.getByText(/Welcome to the UD CIS Scheduler/);
     //const udCisScheduler = screen.getByText(/UD CIS/);
     expect(welcomeMessage).toBeInTheDocument();
