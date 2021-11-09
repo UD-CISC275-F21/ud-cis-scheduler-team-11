db7e45d16265afc1aa4cdeb414fa5aa95458a1e7
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-11-09 10:05:41 -0500

    Delete Override.tsx

--- src/components/Override.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-type BaseProps = ReactNative.TextInputProps;
-interface Table extends BaseProps {
-  name: string;
-  onChangeText: (args: { name: string; value: string }) => void;
-  style: ReactNative.TextStyle;
-}
