6a3c71b86d78e80d31dfe2ef285331831e0dddaf
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-11-09 11:17:30 -0500

    Update Fulfill.tsx

--- src/components/Fulfill.tsx
+++ src/components/Fulfill.tsx
@@ -7,27 +7,27 @@ export function LoadClasses(): JSX.Element{
     const coreClasses=[
         {
             id: "test1",
-            text: "EGGG101 satisfies the First Year Seminar (FYS).",
+            name: "EGGG101 satisfies the First Year Seminar (FYS).",
         },
         {
             id: "test2",
-            text: "ENGL110 is required for all students and should be taken in the first year.",
+            name: "ENGL110 is required for all students and should be taken in the first year.",
         },
         {
             id: "test3",
-            text: "The CAH, HCC, and SBS breadth requirements are required for the College, University Breadth, and University Requirements.",
+            name: "The CAH, HCC, and SBS breadth requirements are required for the College, University Breadth, and University Requirements.",
         },
         {
             id: "test4",
-            text: "2 science courses with lab can satisfy one laboratory science sequence.",
+            name: "2 science courses with lab can satisfy one laboratory science sequence.",
         },
         {
             id: "test5",
-            text: "2 science courses with lab are required for the science course requirements.",
+            name: "2 science courses with lab are required for the science course requirements.",
         },
         {  
             id: "test6",
-            text: "The Multicultural requirement is required for the University Requirements.",
+            name: "The Multicultural requirement is required for the University Requirements.",
         },  
     ];
     /*
@@ -35,54 +35,54 @@ export function LoadClasses(): JSX.Element{
         const coreClasses=[ 
             {
                 id: "test1",
-                text: "EGGG101 satisfies the First Year Seminar (FYS).",
+                name: "EGGG101 satisfies the First Year Seminar (FYS).",
             },
             {
                 id: "test2",
-                text: "ENGL110 is required for all students and should be taken in the first year.",
+                name: "ENGL110 is required for all students and should be taken in the first year.",
             },
             {
                 id: "test3",
-                text: "The CAH, HCC, and SBS breadth requirements are required for the College, University Breadth, and University Requirements.",
+                name: "The CAH, HCC, and SBS breadth requirements are required for the College, University Breadth, and University Requirements.",
             },
             {
                 id: "test4",
-                text: "2 science courses with lab can satisfy one laboratory science sequence.",
+                name: "2 science courses with lab can satisfy one laboratory science sequence.",
             },
             {
                 id: "test5",
-                text: "2 science courses with lab are required for the science course requirements.",
+                name: "2 science courses with lab are required for the science course requirements.",
             },
             {  
                 id: "test6",
-                text: "The Multicultural requirement is required for the University Requirements.",
+                name: "The Multicultural requirement is required for the University Requirements.",
             },
         ];
     }else{
         const coreClasses=[
             {
                 id: "test1",
-                text: "EGGG101 satisfies the First Year Seminar (FYS).",
+                name: "EGGG101 satisfies the First Year Seminar (FYS).",
             },
             {
                 id: "test2",
-                text: "ENGL110 is required for all students and should be taken in the first year.",
+                name: "ENGL110 is required for all students and should be taken in the first year.",
             },
             {
                 id: "test3",
-                text: "The CAH, HCC, and SBS breadth requirements are required for the College, University Breadth, and University Requirements.",
+                name: "The CAH, HCC, and SBS breadth requirements are required for the College, University Breadth, and University Requirements.",
             },
             {
                 id: "test4",
-                text: "2 science courses with lab can satisfy one laboratory science sequence.",
+                name: "2 science courses with lab can satisfy one laboratory science sequence.",
             },
             {
                 id: "test5",
-                text: "2 science courses with lab are required for the science course requirements.",
+                name: "2 science courses with lab are required for the science course requirements.",
             },
             {  
                 id: "test6",
-                text: "The Multicultural requirement is required for the University Requirements.",
+                name: "The Multicultural requirement is required for the University Requirements.",
             },
         ];
     }
