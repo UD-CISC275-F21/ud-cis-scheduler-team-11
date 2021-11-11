b749a5df86c2fe5ec610a6e1dd2907a903c7fa7a
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-11-11 11:47:05 -0500

    added the remaining classes into the course list

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -23,17 +23,17 @@ export const appData: AppState = {
         {
             id: "0",
             text: "Course List",
-            courses: [{id: "c0", text: "CISC106" },{id: "c1", text: "CISC108"},{id: "c2", text: "EGGG101"},{id: "c3", text: "MATH241"},{id: "c4", text: "ENGL110"},{id: "c5", text: "Breadth Requirement (CAH)"},{id: "c6", text: "CISC181"},{id: "c7", text: "CISC210"},{id: "c8", text: "MATH242"},{id: "c9", text: "CISC181"},{id: "c10", text: "Breadth Requirement (HCC)"},{id: "c11", text: "Breadth Requirement (SBS)"},{id: "c12", text: "CISC220"},{id: "c13", text: "CISC260"},{id: "c14", text: "MATH210"},{id: "c15", text: "Science Requirement 1"},{id: "c16", text: "MATH243"},{id: "c17", text: "CISC355"},{id: "c18", text: "CISC275"},{id: "c19", text: "MATH205"},{id: "c20", text: "MATH350"},{id: "c21", text: "Science Requirement 2"},{id: "c22", text: "Breadth Requirement (Multicultural)"}]
+            courses: [{id: "c0", text: "CISC106" },{id: "c1", text: "CISC108"},{id: "c2", text: "EGGG101"},{id: "c3", text: "MATH241"},{id: "c4", text: "ENGL110"},{id: "c5", text: "Breadth Requirement (CAH)"},{id: "c6", text: "CISC181"},{id: "c7", text: "CISC210"},{id: "c8", text: "MATH242"},{id: "c9", text: "Breadth Requirement (HCC)"},{id: "c10", text: "Breadth Requirement (SBS)"},{id: "c11", text: "CISC220"},{id: "c12", text: "CISC260"},{id: "c13", text: "MATH210"},{id: "c14", text: "Science Requirement 1"},{id: "c15", text: "MATH243"},{id: "c16", text: "CISC355"},{id: "c17", text: "CISC275"},{id: "c18", text: "MATH205"},{id: "c19", text: "MATH350"},{id: "c20", text: "Science Requirement 2"},{id: "c21", text: "Breadth Requirement (Multicultural)"}]
         },
         {
             id: "1",
             text: "Year 1: Semester 1",
-            courses: [{ id: "c23", text: "Learn Typescript" }]
+            courses: [{ id: "c22", text: "Learn Typescript" }]
         },
         {
             id: "2",
             text: "Year 1: Semester 2",
-            courses: [{ id: "c24", text: "Begin to use static typing" }]
+            courses: [{ id: "c23", text: "Begin to use static typing" }]
         } 
     ]
 };
