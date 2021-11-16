0329c2885661b3affaa01d9ebc2a31d3f24f23b4
strrules105 <camobot3@gmail.com>
2021-11-16 10:23:20 -0500

    Cleaned up more code, looking to fix known bug with drag and drop

--- src/state/appStateReducer.tsx
+++ src/state/appStateReducer.tsx
@@ -4,20 +4,20 @@ import { findItemIndexById, moveItem } from "../utils/arrayUtils";
 import { DragItem } from "../DragItem";
 import { Action } from "./actions";
 
-export type Task = {
+export type Course = {
     id: string
     text: string
 }
 
-export type List = {
+export type Semester = {
     id: string
     text: string
-    courses: Task[]
+    courses: Course[]
 }
 
 export type AppState = {
     draggedItem: DragItem | null
-    lists: List[]
+    semesters: Semester[]
 }
 
 export const appStateReducer = (
@@ -29,47 +29,47 @@ export const appStateReducer = (
         draft.draggedItem = action.payload;
         break;
     }
-    case "ADD_LIST": {
+    case "ADD_SEMESTER": {
         const id=nanoid();
-        draft.lists.push({
+        draft.semesters.push({
             id: id,
             text: action.payload,
             courses: [],
         });
         break;
     }
-    case "EDIT_LIST": {
+    case "EDIT_SEMESTER": {
         const {text, id } = action.payload;
-        const index = draft.lists.findIndex(list => list.id === id);
-        draft.lists[index].text=text;
+        const index = draft.semesters.findIndex(semester => semester.id === id);
+        draft.semesters[index].text=text;
         break;
     }
-    case "ADD_TASK": {
-        const { text, listId } = action.payload;
-        const targetListIndex = findItemIndexById(draft.lists, listId);
-        draft.lists[targetListIndex].courses.push({
+    case "ADD_COURSE": {
+        const { text, semesterId } = action.payload;
+        const targetSemesterIndex = findItemIndexById(draft.semesters, semesterId);
+        draft.semesters[targetSemesterIndex].courses.push({
             id: nanoid(),
             text
         });
         break;
     }
-    case "DELETE_LIST": {
+    case "DELETE_SEMESTER": {
         const id = action.payload;
-        draft.lists=draft.lists.filter(list => list.id !== id);
+        draft.semesters=draft.semesters.filter(semester => semester.id !== id);
         break;
     }
-    case "DELETE_ALL_LISTS": {
-        draft.lists=draft.lists.slice(0,1);
+    case "DELETE_ALL_SEMESTERS": {
+        draft.semesters=draft.semesters.slice(0,1);
         break;
     }
-    case "MOVE_LIST": {
+    case "MOVE_SEMESTER": {
         const { draggedId, hoverId } = action.payload;
-        const dragIndex = findItemIndexById(draft.lists, draggedId);
-        const hoverIndex = findItemIndexById(draft.lists, hoverId);
-        draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
+        const dragIndex = findItemIndexById(draft.semesters, draggedId);
+        const hoverIndex = findItemIndexById(draft.semesters, hoverId);
+        draft.semesters = moveItem(draft.semesters, dragIndex, hoverIndex);
         break;
     }
-    case "MOVE_TASK": {
+    case "MOVE_COURSE": {
         const {
             draggedItemId,
             hoveredItemId,
@@ -77,33 +77,33 @@ export const appStateReducer = (
             targetColumnId
         } = action.payload;
 
-        const sourceListIndex = findItemIndexById(
-            draft.lists,
+        const sourceSemesterIndex = findItemIndexById(
+            draft.semesters,
             sourceColumnId
         );
-        const targetListIndex = findItemIndexById(
-            draft.lists,
+        const targetSemesterIndex = findItemIndexById(
+            draft.semesters,
             targetColumnId
         );
 
         const dragIndex = findItemIndexById(
-            draft.lists[sourceListIndex].courses,
+            draft.semesters[sourceSemesterIndex].courses,
             draggedItemId
         );
 
         const hoverIndex = hoveredItemId
             ? findItemIndexById(
-                draft.lists[targetListIndex].courses,
+                draft.semesters[targetSemesterIndex].courses,
                 hoveredItemId
             )
             : 0;
-        const item = draft.lists[sourceListIndex].courses[dragIndex];
+        const item = draft.semesters[sourceSemesterIndex].courses[dragIndex];
 
-        // Remove the task from the source list
-        draft.lists[sourceListIndex].courses.splice(dragIndex, 1);
+        // Remove the task from the source semester
+        draft.semesters[sourceSemesterIndex].courses.splice(dragIndex, 1);
 
-        // Add the task to the target list
-        draft.lists[targetListIndex].courses.splice(hoverIndex, 0, item);
+        // Add the task to the target semester
+        draft.semesters[targetSemesterIndex].courses.splice(hoverIndex, 0, item);
         break;
     }
     default: {
