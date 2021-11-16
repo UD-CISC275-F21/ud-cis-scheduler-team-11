efb5a6dc843e214a17c9f93839f235bc452ee6eb
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-11-16 01:51:05 -0500

    Update SemesterContainer.tsx

--- src/components/SemesterContainer.tsx
+++ src/components/SemesterContainer.tsx
@@ -18,7 +18,7 @@ export function SemesterContainer(): JSX.Element {
             )}
             <Row>
                 <AddNewCourse
-                    toggleButtonText="+ Add another term"
+                    toggleButtonText="+ Add another semester"
                     onAdd={text => dispatch(addList(text))}
                 />
                 <Button style={styles.deleteButton} onClick={() => dispatch(deleteLists())}>
