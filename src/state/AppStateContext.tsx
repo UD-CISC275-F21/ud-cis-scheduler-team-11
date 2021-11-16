343af80d8e86d2f5e746c921ea260dfee4c2d8cf
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-11-16 10:23:35 -0500

    Update AppStateContext.tsx

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -22,18 +22,18 @@ export const appData: AppState = {
     lists: [
         {
             id: "0",
-            text: "Course List",
-            courses: [{id: "c1", text: "CISC108: 3 credits"},{id: "c2", text: "EGGG101: 2 credits"},{id: "c3", text: "MATH241: 4 credits"},{id: "c4", text: "ENGL110: 3 credits"},{id: "c5", text: "Breadth Requirement (CAH) Course: 3 credits"},{id: "c6", text: "CISC181: 3 credits"},{id: "c7", text: "CISC210: 3 credits"},{id: "c8", text: "MATH242: 4 credits"},{id: "c9", text: "Breadth Requirement (HCC) Course: 3 credits"},{id: "c10", text: "Breadth Requirement (SBS) Course: 3 credits"},{id: "c11", text: "CISC220: 3 credits"},{id: "c12", text: "CISC260: 3 credits"},{id: "c13", text: "MATH210: 3 credits"},{id: "c14", text: "Science Requirement Course 1: 4 credits"},{id: "c15", text: "MATH243: 4 credits"},{id: "c16", text: "CISC355: 3 credits"},{id: "c17", text: "CISC275: 3 credits"},{id: "c18", text: "MATH205: 4 credits"},{id: "c19", text: "MATH350: 3 credits"},{id: "c20", text: "Science Requirement Course 2: 4 credits"},{id: "c21", text: "Breadth Requirement (MC) Course: 3 credits"}]
+            text: "Degree Requirements",
+            courses: [{id: "c1", text: "CISC108 is a prerequisite for CISC181 and CISC210."},{id: "c2", text: "MATH241 is a prerequisite for MATH242."},{id: "c3", text: "CISC210 is a prerequisite for CISC220 and CISC260."},{id: "c4", text: "MATH241 and MATH242 are prerequisites for MATH210."},{id: "c5", text: "MATH242 is a prerequisite for MATH243."},{id: "c6", text: "CISC181 and CISC220 are prerequisites for CISC275."},{id: "c7", text: "MATH210 is a prerequisite for MATH205."},{id: "c8", text: "MATH243 is a prerequisite for MATH350."}]
         },
         {
             id: "1",
-            text: "Degree Requirements",
-            courses: [{id: "c22", text: "CISC108 is a prerequisite for CISC181 and CISC210."},{id: "c23", text: "MATH241 is a prerequisite for MATH242."},{id: "c24", text: "CISC210 is a prerequisite for CISC220 and CISC260."},{id: "c25", text: "MATH241 and MATH242 are prerequisites for MATH210."},{id: "c26", text: "MATH242 is a prerequisite for MATH243."},{id: "c27", text: "CISC181 and CISC220 are prerequisites for CISC275."},{id: "c28", text: "MATH210 is a prerequisite for MATH205."},{id: "c29", text: "MATH243 is a prerequisite for MATH350."}]
+            text: "Course Prerequisites",
+            courses: [{id: "c9", text: "EGGG101 satisfies the First Year Seminar (FYS)."},{id: "c10", text: "ENGL110 is required for all students and should be taken in the first year."},{id: "c11", text: "The CAH, HCC, and SBS breadth, and Multicultural requirements are required for the College, University Breadth, and University Requirements."},{id: "c12", text: "2 science courses with labs can satisfy one laboratory science sequence."},{id: "c13", text: "A laboratory science sequence is required for the science courses."}]
         },
         {
             id: "2",
-            text: "Course Prerequisites",
-            courses: [{id: "c30", text: "EGGG101 satisfies the First Year Seminar (FYS)."},{id: "c31", text: "ENGL110 is required for all students and should be taken in the first year."},{id: "c32", text: "The CAH, HCC, and SBS breadth, and Multicultural requirements are required for the College, University Breadth, and University Requirements."},{id: "c33", text: "2 science courses with labs can satisfy one laboratory science sequence."},{id: "c34", text: "A laboratory science sequence is required for the science courses."}]
+            text: "Course List",
+            courses: [{id: "c14", text: "CISC108: 3 credits"},{id: "c15", text: "EGGG101: 2 credits"},{id: "c16", text: "MATH241: 4 credits"},{id: "c17", text: "ENGL110: 3 credits"},{id: "c18", text: "Breadth Requirement (CAH) Course: 3 credits"},{id: "c19", text: "CISC181: 3 credits"},{id: "c20", text: "CISC210: 3 credits"},{id: "c21", text: "MATH242: 4 credits"},{id: "c22", text: "Breadth Requirement (HCC) Course: 3 credits"},{id: "c23", text: "Breadth Requirement (SBS) Course: 3 credits"},{id: "c24", text: "CISC220: 3 credits"},{id: "c25", text: "CISC260: 3 credits"},{id: "c26", text: "MATH210: 3 credits"},{id: "c27", text: "Science Requirement Course 1: 4 credits"},{id: "c28", text: "MATH243: 4 credits"},{id: "c29", text: "CISC355: 3 credits"},{id: "c30", text: "CISC275: 3 credits"},{id: "c31", text: "MATH205: 4 credits"},{id: "c32", text: "MATH350: 3 credits"},{id: "c33", text: "Science Requirement Course 2: 4 credits"},{id: "c34", text: "Breadth Requirement (MC) Course: 3 credits"}]
         },
         {
             id: "3",
