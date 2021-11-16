b3bff34feb7ad018a7afa0211c347b4d96a24f09
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-11-16 14:09:17 -0500

    Add files via upload

---
+++ src/state/CoursePrerequisites.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import DropdownBox from "react-dropdown";
+
+
+export function MajorDropdown(): JSX.Element {
+    const options =["CISC108 is a prerequisite for CISC181 and CISC210.", "MATH241 is a prerequisite for MATH242.", "CISC210 is a prerequisite for CISC220 and CISC260.", "MATH241 and MATH242 are prerequisites for MATH210.", "MATH242 is a prerequisite for MATH243.", "CISC181 and CISC220 are prerequisites for CISC275.", "MATH210 is a prerequisite for MATH205.", "MATH243 is a prerequisite for MATH350."];
+    const defaultOption = "Click Here";
+    return(
+        <div style={styles.dropdown}>
+            <h3>Course Prerequisites</h3>
+            <DropdownBox options={options} value={defaultOption} placeholder="Select an option" />
+        </div>
+    );
+}
+
+const styles = {
+    dropdown: {
+        width: "40%",
+        //padding: "0px 0px",
+        margin: "10px auto",
+        background: "tan",
+        borderRadius: "111px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontSize: 20,
+        border: "10px solid black",
+    },
+} as const;
