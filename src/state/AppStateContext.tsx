cc145ae99d6fbb6db96199a80896581fc92291d2
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-11-22 14:27:52 -0500

    Update AppStateContext.tsx

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -23,17 +23,17 @@ export const appData: AppState = {
         {
             id: "0",
             text: "Required Course List",
-            courses: [{id: "c1", text: "CISC220: 3 credits"},{id: "c2", text: "CISC260: 3 credits"},{id: "c3", text: "MATH210: 3 credits"},{id: "c4", text: "MATH243: 4 credits"},{id: "c5", text: "CISC275: 3 credits"},{id: "c6", text: "CISC355: 3 credits"},{id: "c7", text: "MATH205: 4 credits"},{id: "c8", text: "MATH350: 3 credits"},{id: "c9", text: "Science & Lab 2: 4 credits"},{id: "c10", text: "Multicultural: 3 credits"}]
+            courses: [{id: "c1", text: "CISC220: 3 credits"},{id: "c2", text: "CISC260: 3 credits"},{id: "c3", text: "MATH210: 3 credits"},{id: "c4", text: "MATH243: 4 credits"},{id: "c5", text: "Science & Lab 1: 4 credits"},{id: "c6", text: "CISC275: 3 credits"},{id: "c7", text: "CISC355: 3 credits"},{id: "c8", text: "MATH205: 4 credits"},{id: "c9", text: "MATH350: 3 credits"},{id: "c10", text: "Science & Lab 2: 4 credits"},{id: "c11", text: "Multicultural: 3 credits"}]
         },
         {
             id: "1",
             text: "Year 1: Fall Semester",
-            courses: [{id: "c11", text: "CISC108: 3 credits"},{id: "c12", text: "EGGG101: 2 credits"},{id: "c13", text: "MATH241: 4 credits"},{id: "c14", text: "ENGL110: 3 credits"},{id: "c15", text: "CAH: 3 credits"},{id: "c16", text: "Science & Lab 1: 4 credits"}]
+            courses: [{id: "c12", text: "CISC108: 3 credits"},{id: "c13", text: "EGGG101: 2 credits"},{id: "c14", text: "MATH241: 4 credits"},{id: "c15", text: "ENGL110: 3 credits"},{id: "c16", text: "CAH: 3 credits"}]
         },
         {
             id: "2",
             text: "Year 1: Spring Semester",
-            courses: [{id: "c17", text: "CISC181: 3 credits"},{id: "c18", text: "CISC210: 3 credits"},{id: "c19", text: "MATH242: 4 credits"},{id: "c20", text: "HCC: 3 credits"},{id: "c21", text: "SBS: 3 credits"},{id: "c22", text: "DLE: 3 credits"}]
+            courses: [{id: "c17", text: "CISC181: 3 credits"},{id: "c18", text: "CISC210: 3 credits"},{id: "c19", text: "MATH242: 4 credits"},{id: "c20", text: "HCC: 3 credits"},{id: "c21", text: "SBS: 3 credits"}]
         } 
     ]
 };
