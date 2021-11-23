60e3eee4e325e85c5bfde19ec405e5b60a58e753
Noah Trungold <trungold@udel.edu>
2021-11-23 16:15:59 -0500

    adding the modal to the app

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,14 @@ import { MajorDropdown } from "./components/MajorDropdown";
 import { SemesterContainer } from "./components/SemesterContainer";
 import { Footer } from "./components/Footer";
 import { Space } from "./components/SpaceBetweenBodyandFooter";
-
+import { Modal } from "./PopupTutorial/Modal";
 export const App = ():JSX.Element  => {
     return (
         <div className = "App">
             <WelcomePopup />
             <Welcome />
             <MajorDropdown />
+            <Modal />
             <SemesterContainer />
             <Footer />
             <Space />
