e283a5fa10aef01c56742e198756b34f506eda34
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-12-15 15:35:10 -0500

    Update RequirementsTable.tsx

--- src/components/RequirementsTable.tsx
+++ src/components/RequirementsTable.tsx
@@ -22,6 +22,9 @@ export function RequirementsTable(): JSX.Element {
                     <ul  style={styles.Courses}>
                         <p>An 8 credit lab sequence from any of the following: PHYS, CHEM, GEOL, or BISC is required for science courses.</p>
                     </ul>
+                    <ul  style={styles.Courses}>
+                        <p>A minimum grade of C- is required for each of the following courses: CISC108, CISC181, CISC210, CISC220, and CISC260.</p>
+                    </ul>
                 </Col>
 
                 <Col style={styles.border}>
@@ -29,25 +32,31 @@ export function RequirementsTable(): JSX.Element {
                         <h5>Course Prerequisites</h5>
                     </ul>
                     <ul  style={styles.Courses}>
-                        <p>CISC108 is a prerequisite for CISC181 & CISC210.</p>
+                        <p>CISC108 is a prerequisite for CISC181 and CISC210.</p>
                     </ul>
                     <ul  style={styles.Courses}>
                         <p>MATH241 is a prerequisite for MATH242.</p>
                     </ul>
                     <ul  style={styles.Courses}>
-                        <p>MATH241 & MATH242 are corequisites for MATH210.</p>
+                        <p>MATH241 and MATH242 are corequisites for MATH210.</p>
                     </ul>
                     <ul  style={styles.Courses}>
                         <p>MATH242 is a prerequisite for MATH243.</p>
                     </ul>
                     <ul  style={styles.Courses}>
-                        <p>CISC181 & CISC220 are prerequisites for CISC275.</p>
+                        <p>CISC181 and CISC220 are prerequisites for CISC275.</p>
                     </ul>
                     <ul  style={styles.Courses}>
                         <p>MATH210 is a prerequisite for MATH205.</p>
                     </ul>
                     <ul  style={styles.Courses}>
                         <p>MATH243 is a prerequisite for MATH350.</p>
+                    </ul>
+                    <ul  style={styles.Courses}>
+                        <p>MATH210 and CISC220 are prerequisites for CISC303 and CISC320.</p>
+                    </ul>
+                    <ul  style={styles.Courses}>
+                        <p>CISC220 and CISC260 are prerequisites for CISC361 and CISC372.</p>
                     </ul>
                 </Col>
             </Row>
