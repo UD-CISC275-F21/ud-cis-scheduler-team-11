b8e5f90990a9c99cb0e6dcb5de3ca4d732063ba7
James Wan <70609875+jameswan141@users.noreply.github.com>
2021-12-16 17:00:46 -0500

    Update AppStateContext.tsx

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -23,17 +23,17 @@ export const appData: AppState = {
         {
             id: "0",
             text: "Required Course List",
-            courses: [{id: "c1", text: "Multicultural: 3 credits"},{id: "c2", text: "CISC220: 3 credits"},{id: "c3", text: "CISC260: 3 credits"},{id: "c4", text: "CISC275: 3 credits"},{id: "c5", text: "CISC303: 3 credits"},{id: "c6", text: "CISC320: 3 credits"},{id: "c7", text: "CISC355: 3 credits"},{id: "c8", text: "CISC361: 3 credits"},{id: "c9", text: "CISC372: 3 credits"},{id: "c10", text: "MATH210: 3 credits"},{id: "c11", text: "MATH243: 4 credits"},{id: "c12", text: "MATH205: 4 credits"},{id: "c13", text: "MATH350: 3 credits"},{id: "c14", text: "Science and Lab 1: 4 credits"},{id: "c15", text: "Science and Lab 2: 4 credits"},{id: "c16", text: "ENGL312: 3 credits"},{id: "c17", text: "ENGL410: 3 credits"}]
+            courses: [{id: "c1", text: "CISC220: 3 credits"},{id: "c2", text: "CISC260: 3 credits"},{id: "c3", text: "CISC275: 3 credits"},{id: "c4", text: "CISC303: 3 credits"},{id: "c5", text: "CISC320: 3 credits"},{id: "c6", text: "CISC361: 3 credits"},{id: "c7", text: "CISC372: 3 credits"},{id: "c8", text: "MATH210: 3 credits"},{id: "c9", text: "MATH243: 4 credits"},{id: "c10", text: "MATH205: 4 credits"},{id: "c11", text: "MATH350: 3 credits"},{id: "c12", text: "Science and Lab 1: 4 credits"},{id: "c13", text: "Science and Lab 2: 4 credits"},{id: "c14", text: "ENGL312: 3 credits"},{id: "c15", text: "ENGL410: 3 credits"}]
         },
         {
             id: "1",
             text: "Year 1: Fall Semester",
-            courses: [{id: "c18", text: "CISC108: 3 credits"},{id: "c19", text: "EGGG101: 2 credits"},{id: "c20", text: "MATH241: 4 credits"},{id: "c21", text: "ENGL110: 3 credits"},{id: "c22", text: "CAH: 3 credits"}]
+            courses: [{id: "c16", text: "CISC108: 3 credits"},{id: "c17", text: "EGGG101: 2 credits"},{id: "c18", text: "MATH241: 4 credits"},{id: "c19", text: "ENGL110: 3 credits"},{id: "c20", text: "CAH: 3 credits"},{id: "c21", text: "Multicultural: 3 credits"}]
         },
         {
             id: "2",
             text: "Year 1: Spring Semester",
-            courses: [{id: "c23", text: "CISC181: 3 credits"},{id: "c24", text: "CISC210: 3 credits"},{id: "c25", text: "MATH242: 4 credits"},{id: "c26", text: "HCC: 3 credits"},{id: "c27", text: "SBS: 3 credits"}]
+            courses: [{id: "c22", text: "CISC181: 3 credits"},{id: "c23", text: "CISC210: 3 credits"},{id: "c24", text: "MATH242: 4 credits"},{id: "c25", text: "HCC: 3 credits"},{id: "c26", text: "SBS: 3 credits"},{id: "c27", text: "CISC355: 3 credits"}]
         } 
     ]
 };
